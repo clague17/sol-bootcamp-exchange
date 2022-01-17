2953619808ee80998c15d4b7485310d6185c01bb
clague <lclague9@gmail.com>
2022-01-17 10:34:04 -0700

    finished deposit, just have to test

--- program/src/processor/deposit.rs
+++ program/src/processor/deposit.rs
@@ -1,24 +1,18 @@
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
-    system_instruction,
     entrypoint::ProgramResult,
     msg,
     program_error::ProgramError,
     pubkey::Pubkey,
-    system_program::ID as SYSTEM_PROGRAM_ID,
-    sysvar::{rent::Rent, Sysvar},
-    program::{invoke_signed},
+    program::{invoke},
 };
 
-use spl_token::state::Account as TokenAccount;
-use spl_token::state::Mint as Mint;
 
 use crate::{
-    error::ExchangeBoothError,
     state::ExchangeBooth,
 };
 
-use borsh::{BorshDeserialize, BorshSerialize};
+use borsh::{BorshDeserialize};
 
 pub fn assert_with_msg(statement: bool, err: ProgramError, msg: &str) -> ProgramResult {
     if !statement {
@@ -33,7 +27,8 @@ pub fn assert_with_msg(statement: bool, err: ProgramError, msg: &str) -> Program
 pub fn process(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    amount: u8
+    amount_a: u8,
+    amount_b: u8
 ) -> ProgramResult {
     /* 
     * This program will take in the admin account and use the token program to move funds
@@ -41,13 +36,15 @@ pub fn process(
     * The input "amount" will be the WHOLE number of tokens to deposit into vaults A and B
     */
     let accounts_iter = &mut accounts.iter();
+
     let eb_ai = next_account_info(accounts_iter)?;
     let admin_ai = next_account_info(accounts_iter)?;
+    let admin_token_a_acc = next_account_info(accounts_iter)?;
+    let admin_token_b_acc = next_account_info(accounts_iter)?;
     let vault_a = next_account_info(accounts_iter)?;
     let vault_b = next_account_info(accounts_iter)?;
     let mint_a = next_account_info(accounts_iter)?;
     let mint_b = next_account_info(accounts_iter)?;
-    let system_program = next_account_info(accounts_iter)?;
     let token_program = next_account_info(accounts_iter)?;
 
     let eb = ExchangeBooth::try_from_slice(&eb_ai.data.borrow())?;
@@ -92,13 +89,44 @@ pub fn process(
     );
 
     // Now we have valid pdas for vault, we need to know how much to deposit where
-    
+    // do a token transfer from admin to vault a for amount_a
+    let transfer_from_admin_to_vault_a_ix = spl_token::instruction::transfer(
+        token_program.key,
+        admin_token_a_acc.key,
+        &vault_a_pda,
+        admin_ai.key, // The owner of the admin account
+        &[&admin_ai.key],
+        amount_a.try_into().unwrap()
+    )?;
 
+    let transfer_from_admin_to_vault_b_ix = spl_token::instruction::transfer(
+        token_program.key,
+        admin_token_b_acc.key,
+        &vault_b_pda,
+        admin_ai.key, // The owner of the admin account
+        &[&admin_ai.key],
+        amount_b.try_into().unwrap()
+    )?;
 
+    msg!("Transfering {} tokens from admin to vault_a: {}", amount_a, vault_a_pda);
 
-    // verify that the admin is the correct admin
-    // find pda for vault a and vault b
-    // 
+    invoke(
+        &transfer_from_admin_to_vault_a_ix,
+        &[
+            admin_ai.clone(), admin_token_a_acc.clone(), vault_a.clone(), token_program.clone(),
+        ],
+    )?;
+
+    msg!("Transfering {} tokens from admin to vault_b: {}", amount_b, vault_b_pda);
+
+    invoke(
+        &transfer_from_admin_to_vault_a_ix,
+        &[
+            admin_ai.clone(), admin_token_a_acc.clone(), vault_a.clone(), token_program.clone(),
+        ],
+    )?;
+
+    msg!("Succesful deposit!\n");
 
     Ok(())
 }
