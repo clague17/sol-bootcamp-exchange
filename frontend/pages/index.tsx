78e2516654844f4e59550cfa1af1fac2c70f36aa
clague <lclague9@gmail.com>
2022-01-17 22:51:37 -0700

    tried to move to tailwind but linter still not working

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,97 +1,137 @@
-import React, { useEffect, useState } from "react";
-import type { NextPage } from "next";
-import NextImage from "next/image";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { Heading, Text, Stack, Image, Flex, Button } from "@chakra-ui/react";
-import twitterLogo from "../assets/twitter-logo.svg";
-import Pokedex from "../components/Pokedex";
-import TokenView from "../components/TokenView";
+import React, { useEffect, useState } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
 
-// Constants
-const TWITTER_HANDLE = "clague17";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+export default function Home() {
+  const [walletAddress, setWalletAddress] = useState(null);
 
-type FeatureProps = {
-  title: string;
-  desc: string;
-};
+  /*
+   * Declare your function
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
 
-const Feature = (props: FeatureProps) => (
-  <div>
-    <Heading fontSize={"72px"}>{props.title}</Heading>
-    <Text mt={4} fontSize={"36px"}>
-      {props.desc}
-    </Text>
-  </div>
-);
+      if (solana) {
+        if (solana.isPhantom) {
+          // try to connect here
+          const response = await solana.connect({ onlyIfTrusted: true });
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-const connectWallet = async () => {
-  // const { solana } = window;
-  // if (solana) {
-  //   const response = await solana.connect();
-  //   setWalletAddress(response.publicKey.toString());
-  // }
-};
+  /*
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+   */
+  const connectWallet = async () => {
+    const { solana } = window;
 
-const renderNotConnectedContainer = () => {
-  return (
-    <div id="renderNotConnectedContainer" style={{ marginBottom: "70px" }}>
-      <button
-        className="cta-button connect-wallet-button"
-        onClick={connectWallet}
-      >
-        Connect Wallet on Devnet
-      </button>
-    </div>
-  );
-};
+    if (solana) {
+      const response = await solana.connect();
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
 
-const Home: NextPage = () => {
-  const [walletAddress, setWalletAddress] = useState("");
-
-  return (
-    <div className="App" style={{ backgroundColor: "red" }}>
-      <div className={styles.container} style={{ backgroundColor: "red" }}>
-        <Head>
-          <title>pokéDEX</title>
-          <meta name="description" content="Your favorite DEX on Solana" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Stack spacing={8} align="center">
-          <Feature title={"pokéDEX "} desc={"swap your tokens"} />
-          <Image
-            src={
-              "https://media2.giphy.com/media/fSvqyvXn1M3btN8sDh/giphy.gif?cid=ecf05e47f3cmli4l64unl59820kxmcaocd0855t7s1hfsk5v&rid=giphy.gif&ct=g"
-            }
-            boxSize="300px"
-            objectFit="cover"
-            alt="dancing pikachu"
-            borderRadius="20"
-            maxWidth="100%"
-          />
-          {!walletAddress && renderNotConnectedContainer()}
-          <TokenView />
-        </Stack>
-
-        <Flex>
-          <NextImage
-            alt="Twitter Logo"
-            className="twitter-logo"
-            src={twitterLogo}
-            width="35px"
-            height="35px"
-          />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built by @${TWITTER_HANDLE}`}</a>
-        </Flex>
+  const renderNotConnectedContainer = () => {
+    return (
+      <div id="renderNotConnectedContainer" style={{ marginBottom: "70px" }}>
+        <button
+          className="cta-button connect-wallet-button"
+          onClick={connectWallet}
+        >
+          Connect Wallet on Devnet
+        </button>
       </div>
+    );
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>pokèDEX</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex">
+        <h1 className="xl">hello</h1>
+      </div>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{" "}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js!
+          </a>
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          Get started by editing{" "}
+          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+            pages/index.tsx
+          </code>
+        </p>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          <a
+            href="https://nextjs.org/docs"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Find in-depth information about Next.js features and API.
+            </p>
+          </a>
+
+          <a
+            href="https://nextjs.org/learn"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Learn about Next.js in an interactive course with quizzes!
+            </p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Discover and deploy boilerplate example Next.js projects.
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+        </a>
+      </footer>
     </div>
   );
-};
-
-export default Home;
+}
